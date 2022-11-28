0c52a563349b30922776c241afae5220238117db
Viktor Matuyzo <99718345+Vitek169@users.noreply.github.com>
2022-11-28 14:52:27 +0300

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -7,7 +7,7 @@ def get_log(func):
         date_time = datetime.now()
         func_name = func.__name__
         result = func(*args, **kwargs)
-        with open('decoratorlogs.txt', 'w', encoding='utf-8') as file:
+        with open('decoratorlogs.txt', 'a', encoding='utf-8') as file:
             file.write(f'Дата/время: {date_time}\n'
                        f'Имя функции: {func_name}\n'
                        f'Аргументы: {args, kwargs}\n'
